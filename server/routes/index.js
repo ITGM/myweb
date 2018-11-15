727ef7d4f5acd870f720a864437a90a5abd6be7c
刘甘明 <18770814754@163.com>
2018-11-16 00:25:25 +0800

    awsd

--- server/routes/index.js
+++ server/routes/index.js
@@ -15,10 +15,5 @@ router.get('/api/id', function(req, res, next) {
         name:'id'
     })
 });
-router.get('/api/user', function(req, res, next) {
-    res.json({
-        id:1,
-        name:'user'
-    })
-});
+
 module.exports = router;
