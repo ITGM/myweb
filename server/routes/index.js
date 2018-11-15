55b46844aecd3794ff855ebdf69e403371e1bbff
刘甘明 <18770814754@163.com>
2018-11-15 23:45:28 +0800

    awsd

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,11 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/api', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.json({
       id:1,
       name:'json'
   })
+});
+router.get('/api', function(req, res, next) {
+    res.json({
+        id:2,
+        name:'json'
+    })
 });
 
 module.exports = router;
